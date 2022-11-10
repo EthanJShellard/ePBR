af9d0bcdb7d0df67ea73b4759e6791660ff13b34
Ethan Shellard <ethanshellard@gmail.com>
2022-11-10 13:39:51 +0000

    Introduce VertexArray and VertexBuffer objects

---
+++ src/ePBR/VertexBuffer.h
@@ -0,0 +1,33 @@
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+
+namespace ePBR 
+{
+	class VertexBuffer 
+	{
+	public:
+		void Add(glm::vec2 _value);
+		void Add(float _x, float _y);
+		void Add(glm::vec3 _value);
+		void Add(float _x, float _y, float _z);
+		void Add(glm::vec4 _value);
+		void Add(float _x, float _y, float _z, float _w);
+		void Add(GLfloat _value);
+
+		//Get the number of components in one unit of data for this buffer. (e.g. 3 for glm::vec3)
+		int GetComponents();
+		// Get the location of this vertex buffer. Uploads data to GPU if it has changed since last call.
+		GLuint GetID();
+
+		VertexBuffer();
+		~VertexBuffer();
+	private:
+		GLuint m_id;
+		int m_numComponents;
+		std::vector<GLfloat> m_data;
+		bool m_dirty; //Used to specify whether data is yet to be uploaded to GPU
+	};
+}
