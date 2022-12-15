7a761cd72e8af5c81874a5d5379dee53f3b642e5
Ethan Shellard <ethanshellard@gmail.com>
2022-12-15 14:06:59 +0000

    Add doxygen comments to Material

--- src/ePBR/Material.h
+++ src/ePBR/Material.h
@@ -14,8 +14,16 @@ namespace ePBR
 		std::shared_ptr<Shader> m_shaderProgram;
 
 	public:
+		/// @brief Set this material's shader.
+		/// @param _newShader The new shader
 		virtual void SetShader(std::shared_ptr<Shader> _newShader) = 0;
 
+		/// @brief Apply this material in preparation for drawing something it applies to.
+		/// @param _modelMatrix The model matrix of the object to draw.
+		/// @param _invModelMatrix The inverse model matrix of the object to draw.
+		/// @param _viewMatrix The view matrix which should be used to draw.
+		/// @param _projMatrix The projection matrix which should be used to draw.
+		/// @param _camPos The position of the camera corresponds to the view matrix.
 		virtual void Apply(glm::mat4 _modelMatrix, glm::mat4 _invModelMatrix, glm::mat4 _viewMatrix, glm::mat4 _projMatrix, glm::vec3 _camPos) = 0;
 	};
 }
